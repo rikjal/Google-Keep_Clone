46a469aec3c7294477f4ecda0985e12b8e6b77b3
rikjal <saptarshi.jpg@gmail.com>
2021-03-13 02:05:37 +0530

    Final

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 
-function Footer()
-{
+function Footer() {
     const year = new Date().getFullYear()
     return (
         <>
             <footer className="text-center fixed-bottom">
-                <p> Copyright ©️ {year} </p>
+                <p className="text-light"> Copyright ©️ {year} </p>
             </footer>
         </>
     )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline'
 import Card from '@material-ui/core/Card'
-import { makeStyles } from '@material-ui/core/styles'
+import {makeStyles} from '@material-ui/core/styles'
 
 const useStyles = makeStyles( {
     root: {
-        background: "#BCAAA4",
+        background: "#dae2f8",
+        backgroundImage: "linear-gradient(to bottom, #dae2f8, #d6a4a4)",
         marginTop: 15,
         minWidth: 200,
         maxWidth: 300,
@@ -13,10 +14,8 @@ const useStyles = makeStyles( {
     },
 } )
 
-function Note( props )
-{
-    const deleteNote = () =>
-    {
+function Note( props ) {
+    const deleteNote = () => {
         props.deleteItem( props.id )
     }
     const classes = useStyles()
